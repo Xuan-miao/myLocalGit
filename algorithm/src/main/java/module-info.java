f30933af8fa0567df1337c9f0400c92393bdc5ea
Xuan-miao <zhangdk99@gmail.com>
2022-04-29 21:05:12 +0800

    rm

--- algorithm/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.algorithm {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.algorithm to javafx.fxml;
-    exports com.example.algorithm;
-}
--- algthm/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.algthm {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.algthm to javafx.fxml;
-    exports com.example.algthm;
-}
